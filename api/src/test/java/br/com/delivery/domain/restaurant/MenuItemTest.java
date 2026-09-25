843c16f0bc6472d4ca0f2d41f8e59a78a88b114e
Smeltier <connect.goncalves@gmail.com>
2026-09-24 21:22:51 -0300

    add(domain/test): add tests to initial version of Menu Item

---
+++ api/src/test/java/br/com/delivery/domain/restaurant/MenuItemTest.java
@@ -0,0 +1,63 @@
+package br.com.delivery.domain.restaurant;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MenuItemTest {
+    private final MenuItemId ID = MenuItemId.generate();
+    private final String NAME = "name";
+    private final String DESCRIPTION = "description";
+    private final MenuItemCategory CATEGORY = MenuItemCategory.DESSERT;
+
+    @Test
+    void shouldCreateValidMenuItem() {
+        var item = new MenuItem(ID, NAME, DESCRIPTION, CATEGORY);
+        assertEquals(ID, item.getId());
+        assertEquals(NAME, item.getName());
+        assertEquals(DESCRIPTION, item.getDescription());
+        assertEquals(CATEGORY, item.getCategory());
+    }
+
+    @Test
+    void shouldThrowWhenIdIsNull() {
+        assertThrows(NullPointerException.class,
+                () -> new MenuItem(null, NAME, DESCRIPTION, CATEGORY));
+    }
+
+    @Test
+    void shouldThrowWhenNameIsNull() {
+        assertThrows(NullPointerException.class,
+                () -> new MenuItem(ID, null, DESCRIPTION, CATEGORY));
+    }
+
+    @Test
+    void shouldThrowWhenNameIsBlank() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new MenuItem(ID, "", DESCRIPTION, CATEGORY));
+    }
+
+    @Test
+    void shouldThrowWhenDescriptionIsNull() {
+        assertThrows(NullPointerException.class,
+                () -> new MenuItem(ID, NAME, null, CATEGORY));
+    }
+
+    @Test
+    void shouldThrowWhenDescriptionIsBlank() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new MenuItem(ID, NAME, "", CATEGORY));
+    }
+
+    @Test
+    void shouldThrowWhenCategoryIsNull() {
+        assertThrows(NullPointerException.class,
+                () -> new MenuItem(ID, NAME, DESCRIPTION, null));
+    }
+
+    @Test
+    void shouldBeSameMenuItemWhenIdIsEqual() {
+        var firstMenuItem = new MenuItem(ID, NAME, DESCRIPTION, CATEGORY);
+        var secondMenuItem = new MenuItem(ID, NAME, DESCRIPTION, CATEGORY);
+        assertEquals(firstMenuItem, secondMenuItem);
+    }
+}
